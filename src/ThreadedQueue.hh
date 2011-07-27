411ea2a0754f4eb2b6dc1e321ccc46e90ab26e86
Benoit Favre <benoit.favre@lif.univ-mrs.fr>
2011-07-27 16:00:04 +0200

    new background queue implementation

---
+++ src/ThreadedQueue.hh
@@ -0,0 +1,106 @@
+#pragma once
+
+#include <assert.h>
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+
+/* This class implements a blocking queue of jobs that are exectued by a
+ * background thread.  Results should be stored in std::promise instances so
+ * that you can retrieve them later.  It is mandatory to call drain() before
+ * destroying the object so that the jobs are finished correctly and the thread
+ * is terminated. Example:
+ *
+ * class MyQueue: public ThreadedQueue<std::pair<int,std::promise<int>*>> {
+ *     public:
+ *     MyQueue(): ThreadedQueue<std::pair<int,std::promise<int>*>>(100) { }
+ *     void process(std::pair<int,std::promise<int>*>& i) {
+ *         i.second->set_value(i.first);
+ *     }
+ * };
+ *
+ * int main(int argc, char** argv) {
+ *     MyQueue queue;
+ *     std::vector<std::promise<int>*> results;
+ *     for(int i = 0; i < 200; i++) {
+ *         results.push_back(new std::promise<int>());
+ *         queue.enqueue(std::pair<int,std::promise<int>*>(i, results.back()));
+ *     }
+ *     for(auto i = results.begin(); i != results.end(); ++i) {
+ *         fprintf(stdout, "%d\n", (*i)->get_future().get());
+ *         delete (*i);
+ *     }
+ *     queue.drain();
+ * }
+ */
+
+template<class T>
+class ThreadedQueue {
+    std::thread thread;
+    std::mutex guard;
+    std::condition_variable can_process;
+    std::condition_variable can_enqueue;
+    std::condition_variable queue_empty;
+    int start, end, size, max_size;
+    bool finished, running;
+    T* queue;
+
+    public:
+
+    // _max_size is the maximum size of the queue
+    ThreadedQueue(int _max_size) : start(0), end(0), size(0), max_size(_max_size), finished(false), running(false) {
+        queue = new T[max_size];
+        thread = std::thread(&ThreadedQueue<T>::run, this);
+    }
+
+    // warning: you must call drain before destructing the object
+    ~ThreadedQueue() {
+        delete queue;
+    }
+
+    // this is where you implement stuff
+    virtual void process(T& item) = 0;
+
+    void run() {
+        running = true;
+        while(true) {
+            std::unique_lock<std::mutex> l(guard);
+            while(size == 0) {
+                if(finished) {
+                    running = false;
+                    queue_empty.notify_one();
+                    return;
+                }
+                can_process.wait(l);
+            }
+            process(queue[start]);
+            start = (start + 1) % max_size;
+            size--;
+            can_enqueue.notify_one();
+        }
+    }
+
+    // call enqueue with pairs of input and std::promise to be able to retrieve results
+    void enqueue(T item) {
+        std::unique_lock<std::mutex> l(guard);
+        assert(!finished);
+        while(size == max_size) can_enqueue.wait(l);
+        queue[end] = item;
+        end = (end + 1) % max_size;
+        size++;
+        can_process.notify_one();
+    }
+
+    // finishes processing the queue
+    void drain() {
+        std::unique_lock<std::mutex> l(guard);
+        if(!finished) {
+            finished = true;
+            while(running) {
+                can_process.notify_all(); // unlock thread
+                queue_empty.wait(l); // wait for it to finish
+            }
+            thread.join();
+        }
+    }
+};
--- src/ranker_parallel.cc
+++ src/ranker_parallel.cc
@@ -2,110 +2,63 @@
 #include <string.h>
 #include <stdlib.h>
 #include "Predictor.hh"
-#include "thread_safe_queue.hh"
+#include "ThreadedQueue.hh"
 #include <thread>
 #include <future>
 #include <utility>
 #include <vector>
 #include <string>
 
-class input_thread {
-    thread_safe_queue<std::pair<char*,std::promise<double>*>>* queue;
+class input_thread: public ThreadedQueue<std::pair<char*,std::promise<double>*>> {
     ranker::Predictor& model;
-    std::thread thread;
 
     public:
-    void push(std::pair<char*,std::promise<double>*> item) {
-        queue->push(item);
-    }
-    void start() {
-        while(!queue->finished()) {
-            if(!queue->empty()) {
-                std::pair<char*,std::promise<double>*> input = queue->pop();
-                ranker::Example x;
-                char *inputstring = input.first;
-                char *token = NULL; 
-                token =  strsep(&inputstring, " \t"); // skip label
-                for(;(token = strsep(&inputstring, " \t\n"));) {
-                    if(!strcmp(token,"")) continue;
-                    char* value = strrchr(token, ':');
-                    if(value != NULL) {
-                        *value = '\0';
-                        double value_as_double = strtod(value + 1, NULL);
-                        //nbe is the loss, not a feature
-                        if(!strcmp(token, "nbe")) {
-                            x.loss = value_as_double;
-                        } else {
-                            int location = strtol(token, NULL, 10);
-                            x.features.push_back(ranker::Feature(location, value_as_double));
-                        }
-                    }
+    void process(std::pair<char*,std::promise<double>*>& input) {
+        ranker::Example x;
+        char *inputstring = input.first;
+        char *token = NULL; 
+        token =  strsep(&inputstring, " \t"); // skip label
+        for(;(token = strsep(&inputstring, " \t\n"));) {
+            if(!strcmp(token,"")) continue;
+            char* value = strrchr(token, ':');
+            if(value != NULL) {
+                *value = '\0';
+                double value_as_double = strtod(value + 1, NULL);
+                //nbe is the loss, not a feature
+                if(!strcmp(token, "nbe")) {
+                    x.loss = value_as_double;
+                } else {
+                    int location = strtol(token, NULL, 10);
+                    x.features.push_back(ranker::Feature(location, value_as_double));
                 }
-                double score = model.compute_score(x);
-                input.second->set_value(score);
-                free(input.first);
-            } else {
-                sched_yield();
             }
         }
+        double score = model.compute_score(x);
+        input.second->set_value(score);
+        free(input.first);
     }
 
-    void stop() {
-        queue->set_finished();
-        thread.join();
-    }
-
-    input_thread(ranker::Predictor& _model, int queue_size):model(_model) {
-        queue = new thread_safe_queue<std::pair<char*,std::promise<double>*>>(queue_size);
-        thread = std::thread(&input_thread::start, this);
-    }
-    ~input_thread() {
-        delete queue;
-    }
+    input_thread(ranker::Predictor& _model, int queue_size):ThreadedQueue<std::pair<char*,std::promise<double>*>>(queue_size), model(_model) { }
 };
 
-class output_thread {
-    std::thread thread;
-    thread_safe_queue<std::vector<std::promise<double>*>*>* queue;
-
+class output_thread : public ThreadedQueue<std::vector<std::promise<double>*>*> {
     public:
-    void push(std::vector<std::promise<double>*>* item) {
-        queue->push(item);
-    }
-    void start() {
-        while(!queue->finished()) {
-            if(!queue->empty() && !queue->finished()) {
-                std::vector<std::promise<double>*>* result = queue->pop(); // single consumer
-                int argmax = -1;
-                double max = 0;
-                for(size_t i = 0; i < result->size(); i++) {
-                    double value = (*result)[i]->get_future().get();
-                    if(argmax == -1 || value > max) {
-                        argmax = i;
-                        max = value;
-                    }
-                    delete (*result)[i];
-                }
-                delete result;
-                fprintf(stdout, "%d\n", argmax);
-            } else {
-                sched_yield();
+    void process(std::vector<std::promise<double>*>*& result) {
+        int argmax = -1;
+        double max = 0;
+        for(size_t i = 0; i < result->size(); i++) {
+            double value = (*result)[i]->get_future().get();
+            if(argmax == -1 || value > max) {
+                argmax = i;
+                max = value;
             }
+            delete (*result)[i];
         }
+        delete result;
+        fprintf(stdout, "%d\n", argmax);
     }
 
-    void stop() {
-        queue->set_finished();
-        thread.join();
-    }
-
-    output_thread(int queue_size) {
-        queue = new thread_safe_queue<std::vector<std::promise<double>*>*>(queue_size);
-        thread = std::thread(&output_thread::start, this);
-    }
-    ~output_thread() {
-        delete queue;
-    }
+    output_thread(int queue_size) : ThreadedQueue<std::vector<std::promise<double>*>*>(queue_size) { }
 };
 
 int main(int argc, char** argv) {
@@ -132,20 +85,20 @@ int main(int argc, char** argv) {
     std::vector<std::promise<double>*>* results = new std::vector<std::promise<double>*>();
     while(0 <= (length = getline(&buffer, &buffer_length, stdin))) {
         if(length == 1) {
-            output.push(results);
+            output.enqueue(results);
             results = new std::vector<std::promise<double>*>();
         } else if(num_candidates == -1 || (int) results->size() < num_candidates) {
             std::promise<double>* result = new std::promise<double>();
             results->push_back(result);
-            input[current_input_thread]->push(std::pair<char*,std::promise<double>*>(strdup(buffer), results->back()));
+            input[current_input_thread]->enqueue(std::pair<char*,std::promise<double>*>(strdup(buffer), results->back()));
             current_input_thread++;
             if(current_input_thread >= num_threads) current_input_thread = 0; // round-robin
         }
     }
     for(int i = 0; i < num_threads; i++) {
-        input[i]->stop();
+        input[i]->drain();
         delete input[i];
     }
-    output.stop();
+    output.drain();
     return 0;
 }
