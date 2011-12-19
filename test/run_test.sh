1154c56fba9efa8e996780431433969534ff43ff
jlr <joseph.le.roux@gmail.com>
2011-12-19 13:01:48 +0100

    test : changed "zcat" to "gunzip -c"

--- test/run_test.sh
+++ test/run_test.sh
@@ -16,6 +16,6 @@ do
     [ -f $dataset.$i.mapped.gz ] || zcat $dataset.$i.gz | $bin/filter_and_map $dataset.tr.dict 1 | gzip > $dataset.$i.mapped.gz
 done
 
-[ -f $dataset.model ] || $bin/ranker-learn -s $dataset.tr.mapped.gz -d $dataset.val.mapped.gz -t $dataset.t.mapped.gz -c 1 -i 10 -f "zcat" > $dataset.model
+[ -f $dataset.model ] || $bin/ranker-learn -s $dataset.tr.mapped.gz -d $dataset.val.mapped.gz -t $dataset.t.mapped.gz -c 1 -i 10 -f "pigz -dc" -j 4 > $dataset.model
 zcat $dataset.val.mapped.gz | $bin/ranker_main $dataset.model > $dataset.val.out
 zcat $dataset.t.mapped.gz | $bin/ranker_main $dataset.model > $dataset.t.out
